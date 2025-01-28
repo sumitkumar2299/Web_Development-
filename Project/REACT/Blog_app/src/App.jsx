66cc39386a6f73bb5969087f3be8c1f96358885e
sumitkumar2299 <sumitofficialme@gmail.com>
2025-01-28 19:54:27 +0530

    blog app starter

---
+++ Project/REACT/Blog_app/src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-gray-600 text-center pt-1 pb-1 pl-1 text-white'>building mega project on react </h1>
+    </>
+  )
+}
+
+export default App
